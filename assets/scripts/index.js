1e4b0085a3454ab6de4ea5caff35d85514ca8f71
Murad <agamedov94@mail.ru>
2024-08-18 18:42:59 +0400

    commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,18 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
   const selects = document.querySelectorAll('select');
+  const loadingElement = document.getElementById('loading');
+  const convertButton = document.getElementById('convertButton');
 
-  
+  const toggleLoading = (isLoading) => {
+    loadingElement.style.display = isLoading ? 'block' : 'none';
+    convertButton.disabled = isLoading;
+  };
+
+  toggleLoading(true); 
+
   getData('https://bookingbot.muraddev.com/').then(res => {
     selects.forEach(select => {
       res.forEach(option => {
         const optionElement = document.createElement('option');
-        optionElement.value = option.code; 
+        optionElement.value = option.code;
         optionElement.textContent = `${option.code} - ${option.name}`;
         select.appendChild(optionElement);
       });
     });
+    toggleLoading(false); 
   });
-
 
   document.getElementById('currencyForm').addEventListener('submit', (event) => {
     event.preventDefault();
@@ -22,6 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const toCurrency = document.getElementById('toCurrency').value;
 
     if (amount && fromCurrency && toCurrency) {
+      toggleLoading(true);
+
       getData('https://bookingbot.muraddev.com/').then(res => {
         const fromRate = res.find(item => item.code === fromCurrency).value;
         const toRate = res.find(item => item.code === toCurrency).value;
@@ -32,6 +42,8 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
           document.getElementById('result').textContent = 'Məlumat əldə edilə bilmədi.';
         }
+
+        toggleLoading(false); 
       });
     } else {
       document.getElementById('result').textContent = 'Lütfən, bütün sahələri doldurun.';
@@ -39,8 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-
 const getData = async (url) => {
   const res = await fetch(url);
   return await res.json();
-}
+};
